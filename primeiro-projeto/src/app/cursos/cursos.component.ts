6f5f86e9f7f4141b6ff9d5fbb82ebf0b5ebae551
Erik Seyti <erikseyti@gmail.com>
2020-09-23 16:52:07 -0300

    Criado Component e Service Cursos para o primeiro-projeto

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
+import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  // em javascript não é necessario tipar a variavel, mas se desejar pode tipar ela.
+  // em TS, o padrão correto se não quiser tipar uma variavel é declarar ela como "any".
+  // exemploVar: any;
+  nomePortal: string;
+
+  cursos: string[];
+
+  constructor(cursoService:CursosService) {
+    // this.exemploVar = 5;
+    // this.exemploVar = "teste";
+    this.nomePortal = "http://loiane.training";
+
+    // for (let i=0; i<this.cursos.length; i++)
+    // {
+    //   let curso = this.cursos[i];
+    // }
+
+      //var servico = new CursosService();
+
+
+   }
 
   ngOnInit(): void {
   }
